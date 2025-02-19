8b2cba0c84b31b39b7052226833ad96c443ff7f2
Muhammad Haniff <68419391+Haniff-Toha@users.noreply.github.com>
2025-02-19 13:06:40 +0700

    Update E-Commerce-public-dashboard.py

--- E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/E-Commerce-public-dashboard.py
+++ E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/E-Commerce-public-dashboard.py
@@ -17,10 +17,10 @@ def load_data():
     order_items_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/customers_dataset.csv")
     order_payments_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/customers_dataset.csv")
     order_reviews_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/order_reviews_dataset.csv")
-    orders_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/orders_dataset.csv", parse_dates=['order_purchase_timestamp', 'order_delivered_customer_date'])
-    products_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/products_dataset.csv")
-    product_category_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/product_category_name_translation.csv")
-    sellers_df = pd.read_csv("https://raw.githubusercontent.com/Haniff-Toha/DataScience_Portofolio/refs/heads/main/E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/sellers_dataset.csv")
+    orders_df = pd.read_csv("E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/orders_dataset.csv", parse_dates=['order_purchase_timestamp', 'order_delivered_customer_date'])
+    products_df = pd.read_csv("E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/products_dataset.csv")
+    product_category_df = pd.read_csv("E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/product_category_name_translation.csv")
+    sellers_df = pd.read_csv("E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/sellers_dataset.csv")
     brazil_states = gpd.read_file("E-Commerce-Public-Dataset-EDA-Dashboard/dashboard/brazil_states/BRA_adm1.shp")
     return customers_df, geolocation_df, order_items_df, order_payments_df, order_reviews_df, orders_df, products_df, product_category_df, sellers_df, brazil_states
 
