689365f76543a51529d4779ce591aa2b232526e3
Muhammad Haniff <68419391+Haniff-Toha@users.noreply.github.com>
2025-06-23 00:06:26 +0700

    Update app.py

--- Student_Dropout_Prediction/app.py
+++ Student_Dropout_Prediction/app.py
@@ -4,10 +4,10 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
 # Load model
-model = joblib.load("rf_model.joblib")
+model = joblib.load("https://github.com/Haniff-Toha/DataScience_Portofolio/raw/refs/heads/main/Student_Dropout_Prediction/requirements.txt")
 
 # Load scaler (assuming you've saved it, or redefine the same fit here)
-scaler = joblib.load("scaler.joblib")  # If not saved, manually define it as used before
+scaler = joblib.load("https://github.com/Haniff-Toha/DataScience_Portofolio/raw/refs/heads/main/Student_Dropout_Prediction/scaler.joblib")  # If not saved, manually define it as used before
 
 st.title("🎓 Student Dropout Prediction App")
 
